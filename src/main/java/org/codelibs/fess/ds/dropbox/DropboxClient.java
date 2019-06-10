f88b4d509a251f8f4fd9c19b7e71bc0e54eba772
Ryo Kuramoto <kuramoto@n2sm.net>
2019-06-10 17:11:55 +0900

    add DropboxClient & implement getMembers

---
+++ src/main/java/org/codelibs/fess/ds/dropbox/DropboxClient.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2012-2019 CodeLibs Project and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.codelibs.fess.ds.dropbox;
+
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxTeamClientV2;
+import com.dropbox.core.v2.team.TeamMemberInfo;
+import org.codelibs.core.lang.StringUtil;
+import org.codelibs.fess.exception.DataStoreException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class DropboxClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(DropboxClient.class);
+
+    protected static final String ACCESS_TOKEN = "access_token";
+
+    protected DbxRequestConfig config;
+    protected DbxTeamClientV2 client;
+    protected Map<String, String> params;
+
+    public DropboxClient(final Map<String, String> params) {
+        this.params = params;
+
+        final String accessToken = params.getOrDefault(ACCESS_TOKEN, StringUtil.EMPTY);
+        if (StringUtil.isBlank(accessToken)) {
+            throw new DataStoreException("Parameter '" + ACCESS_TOKEN + "' is required");
+        }
+
+        this.config = new DbxRequestConfig("fess");
+        this.client = new DbxTeamClientV2(config, accessToken);
+    }
+
+    public void getMembers(final Consumer<TeamMemberInfo> consumer) throws DbxException {
+        client.team().membersList().getMembers().forEach(consumer);
+    }
+
+    // TODO get files, download file
+
+}
---
+++ src/test/java/org/codelibs/fess/ds/dropbox/DropboxClientTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2012-2019 CodeLibs Project and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.codelibs.fess.ds.dropbox;
+
+import com.dropbox.core.DbxException;
+import org.dbflute.utflute.lastaflute.LastaFluteTestCase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DropboxClientTest extends LastaFluteTestCase {
+
+    private static final String ACCESS_TOKEN = "";
+
+    @Override
+    protected String prepareConfigFile() {
+        return "test_app.xml";
+    }
+
+    @Override
+    protected boolean isSuppressTestCaseTransaction() {
+        return true;
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void test() throws Exception {
+        // getMembers();
+    }
+
+    private void getMembers() throws DbxException {
+        final Map<String, String> params = new HashMap<>();
+        params.put(DropboxClient.ACCESS_TOKEN, ACCESS_TOKEN);
+        final DropboxClient client = new DropboxClient(params);
+        client.getMembers(info -> System.out.println(info.getProfile().getName().getDisplayName()));
+    }
+
+}
